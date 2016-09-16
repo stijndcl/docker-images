a1081ebed5ee0217d9184e51e0c0bc919901f665
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-09-16 14:41:51 +0200

    add haskell docker image

---
+++ dodona-haskell/logger.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# TODO: shorter time intervals at the start of the script and gradually longer time intervals in later stages
+
+beginTime=`date +%s%3N`
+
+while true; do
+	# store the time for timestamping in the logs
+	currentTime=`date +%s%3N`
+	elapsedTime=$((currentTime - beginTime))
+
+	# load files first
+	memoryStats=`cat '/sys/fs/cgroup/memory/memory.stat'`
+	memoryUsage=`cat '/sys/fs/cgroup/memory/memory.usage_in_bytes'`
+	cpuStats=`cat '/sys/fs/cgroup/cpuacct/cpuacct.stat'`
+
+	# gather metrics
+	activeAnon=`echo "$memoryStats" | grep '^total_active_anon ' | cut -d ' ' -f 2`
+	inactiveAnon=`echo "$memoryStats" | grep '^total_inactive_anon ' | cut -d ' ' -f 2`
+
+	userTime=`echo "$cpuStats" | grep '^user ' | cut -d ' ' -f 2`
+	systemTime=`echo "$cpuStats" | grep '^system ' | cut -d ' ' -f 2`
+
+	anonymousMemory=$(($activeAnon + $inactiveAnon))
+
+	# write to logs
+	printf "%s\t%s\n" "$elapsedTime" "$memoryUsage" >> memory_usage.logs
+	printf "%s\t%s\n" "$elapsedTime" "$activeAnon" >> anonymous_memory.logs 
+	printf "%s\t%s\n" "$elapsedTime" "$anonymousMemory" >> total_anonymous_memory.logs 
+	printf "%s\t%s\n" "$elapsedTime" "$userTime" >> user_time.logs
+	printf "%s\t%s\n" "$elapsedTime" "$systemTime" >> system_time.logs
+
+	# wait a bit
+	sleep 0.05
+done
