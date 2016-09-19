013d4ef8b2eac31d823e2932b0732a61b4d8c604
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-09-19 22:50:50 +0200

    preserve cabal by moving it around

--- dodona-haskell/main.sh
+++ dodona-haskell/main.sh
@@ -11,12 +11,12 @@ chmod -R 555 "${SECRET_PATH}" 2> /dev/null
 # create home directory for user "runner"
 chown runner /home/runner
 chmod 700 /home/runner
+mv /.cabal /home/runner
 
 # make ~runner the current directory
 cd /home/runner
 
 # switch to user "runner" and start the script
-whoami >&2
 su runner -c "PATH='$PATH' ${RUNNER_SCRIPT} ${SECRET_PATH}" <&0
 
 # it's the exit status of the runner script that we want to return
