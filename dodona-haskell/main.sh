b026391af0a6c5ad4d61ae1620b272f629a24ddc
Bart Mesuere <bart.mesuere@ugent.be>
2016-09-20 10:52:11 +0200

    also move .ghc directory

--- dodona-haskell/main.sh
+++ dodona-haskell/main.sh
@@ -12,12 +12,13 @@ chmod -R 555 "${SECRET_PATH}" 2> /dev/null
 chown runner /home/runner
 chmod 700 /home/runner
 mv /.cabal /home/runner
+mv /.ghc /home/runner
 
 # make ~runner the current directory
 cd /home/runner
 
 # switch to user "runner" and start the script
-su runner -c "PATH='$PATH' ${RUNNER_SCRIPT} ${SECRET_PATH}" <&0
+su runner -c "PATH='$PATH' ${RUNNER_SCRIPT}" <&0
 
 # it's the exit status of the runner script that we want to return
 STATUS=$?
