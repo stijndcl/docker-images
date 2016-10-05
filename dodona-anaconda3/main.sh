3f98f3c212ddb63d733b9e9b99f76f1b7e13bd94
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-10-05 14:38:17 +0200

    use homedir as current working directory

--- dodona-anaconda3/main.sh
+++ dodona-anaconda3/main.sh
@@ -4,25 +4,22 @@
 RUNNER_SCRIPT=$1
 LOG_PATH=$2
 
-# make ~runner the current directory
-cd /home/runner
-
 # start memory footprint logging
 /logger.sh "$LOG_PATH" &
 LOGGER_PID=$!
 
-# checkout the working directory
+# prepare the homedir with the workdir files and check it out
 config="$(cat)"
-cd "$(jshon -e 'workdir' -u <<<"$config")"
+homedir="/home/runner"
+workdir="$(jshon -e 'workdir' -u <<<"$config")"
+su runner -c "cp -r '$workdir' '$homedir'"
+cd "$homedir"
 
 # switch to user "runner" and start the script
 su runner -c "PATH='$PATH' ${RUNNER_SCRIPT}" <<<"$config"
-#${RUNNER_SCRIPT} <&0
 
 # it's the exit status of the runner script that we want to return
 STATUS=$?
-
-#echo 'done with judge'
 
 # stop memory footprint logging
 kill ${LOGGER_PID}
--- dodona-haskell/main.sh
+++ dodona-haskell/main.sh
@@ -3,9 +3,12 @@
 # process arguments
 RUNNER_SCRIPT=$1
 
-# Checkout the workdir
+# prepare the homedir with the workdir files and check it out
 config="$(cat)"
-cd "$(jshon -e 'workdir' -u <<<"$config")"
+homedir="/home/runner"
+workdir="$(jshon -e 'workdir' -u <<<"$config")"
+su runner -c "cp -r '$workdir' '$homedir'"
+cd "$homedir"
 
 # switch to user "runner" and start the script
 # the exec code to this script is the exit code of the runner script
