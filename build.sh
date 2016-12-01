703d37ff7d19ec1cda83f24a8a992b6939f7c34b
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2016-12-01 15:34:07 +0100

    let build.sh also clean stale images and containers

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# clean stale docker images
+docker images -q --filter "dangling=true" | xargs docker rmi
+
+# clean stale docker containers
+docker ps -a -q | xargs docker rm
+
 ls *.dockerfile | while read dockerfile; do
     name="${dockerfile%.dockerfile}"
 
-    docker build -t "$name" -f "$dockerfile" .
+    docker build --pull --force-rm -t "$name" -f "$dockerfile" .
 done
