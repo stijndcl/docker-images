75f3ded4132edcbc4f4d964d5bb3dd44e5432879
Inteon <42113979+inteon@users.noreply.github.com>
2021-08-25 11:15:21 +0200

    allow building 1 image using build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,13 @@ docker images -q --filter "dangling=true" | xargs docker rmi
 # clean stale docker containers
 docker ps -a -q | xargs docker rm
 
-ls *.dockerfile | while read dockerfile; do
+docker_files=*.dockerfile
+# use docker files passed by script parameter if provided
+if [ $# -gt 0 ]; then
+    docker_files=$@
+fi
+
+for dockerfile in $docker_files; do
     name="${dockerfile%.dockerfile}"
 
     docker build --pull --force-rm -t "$name" -f "$dockerfile" .
